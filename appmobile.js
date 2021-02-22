058378091343ea22bfe7fa475da28148502df25c
hklausn <hklausn@users.noreply.github.com>
2021-02-22 22:53:31 +0200

    Add files via upload

---
+++ appmobile.js
@@ -0,0 +1,513 @@
+
+
+var question = [
+    
+    {
+        prompt: "מהו תפקיד המפוח במנוע דיזל דו פעימתי?",
+        answer1:"לשאוב את גזי הפליטה מהצילינדר.",
+        answer2:"לספק אוויר בלחץ לצילינדר לשרפת שאריות הדלק.",
+        answer3:"לספק אוויר בלחץ לצילינדר להרחקת אדי הדלק.",
+        answer4:"לספק אויר טרי בלחץ לצילינדר, לפינוי גזים ולשריפה. ",
+        correctAnswer: "d"
+    },
+    {
+
+        prompt: "מה עלול להיגרם כשמרווח השסתומים גדול מדי ?",
+        answer1:"לפעולה רועשת וירידה בהספק.",
+        answer2:"לאיבוד דחיסה ולהתחממות המנוע",
+        answer3:"לשריפה מהירה יותר ועליה בהספק",
+        answer4:"להתנעה קלה יותר.",
+        correctAnswer:"a"
+
+            },
+        {
+
+        prompt: "מה עלול לגרום מרווח שסתומים קטן מידי ?",
+        answer1:"שריפה מהירה יותר",
+        answer2:"פעולה רועשת.",
+        answer3:"שריפת שסתום הפליטה",
+        answer4:"שריפת שסתום היניקה.",
+        correctAnswer:"c"
+        
+        },
+
+        {
+        prompt: "על ידי מה נסגרים פתחי היניקה במנוע בנזין דו פעימתי ?",
+        answer1:"הבוכנה",
+        answer2:"השסתומים",
+        answer3:"השרוול",
+        answer4:"טבעות האטימה.",
+        correctAnswer:"a"
+        },
+        {
+        prompt: "הו תפקידה העיקרי של משקולת גל הארכובה",
+        answer1:"כוח איזון צנטריפוגלי",
+        answer2:"הגדלת משקל גל הארכובה.",
+        answer3:"כוח איזון ללחץ הבערה.",
+        answer4:"הגדלת מומנט הסיבוב.",
+        correctAnswer:"a"
+            },        
+            {
+        prompt: "מה מאפיין את צינוריות הדלק ממשאבת ההזרקה למרססים",
+        answer1:"ישרות ומתוחות.",
+        answer2:"חלקות למניעת מפל לחץ.",
+        answer3:"בעלות אורך שווה. ",
+        answer4:"עמידות בטמפרטורה",
+        correctAnswer:"c"
+        },
+
+            {
+        prompt: "מהו תפקידו של מרסס הדלק אינג'קטור",
+        answer1:"לרסס את השמן לדלק תערובת",
+        answer2:"לרסס את הדלק המוחזר.",
+        answer3:"לרסס את הדלק למשאבת ההזרקה.",
+        answer4:"לרסס את הדלק לתא השריפה",
+        correctAnswer:"d"
+
+        },
+        {
+        prompt: "מה תהיה ההשפעה על מבנה המנוע ככל שמספר הצילינדרים יגדל?",
+        answer1:" אין שום השפעה לכמות הצילינדרים , אלא למשקל.",
+        answer2:" גלגל התנופה יקטן באופן יחסי למספר הצילינדרים.",
+        answer3:" גלגל התנופה יגדל באופן יחסי למספר הצילינדרים",
+        answer4:" כמות הבוכנות תקטן באותו יחס.",
+        correctAnswer:"b"    
+        },
+        {
+        prompt: "מהו המאפיין העיקרי של מנוע דיזל",
+        answer1:" מנוע שריפה פנימית והצתה בדחיסה",
+        answer2:" מנוע שריפה פנימית והצתה בניצוץ",
+        answer3:" מנוע שריפה חיצונית והצתה בדחיסה",
+        answer4:" מנוע שריפה חיצונית של סולר",
+        correctAnswer:"a"      
+        },
+
+        {
+        prompt: "מה נדרש לבצע לפני ההתנעה",
+        answer1: " ניקוי ושימון התשלובות",
+        answer2: " חיבור חשמל ובדיקת התמסורות",
+        answer3: " נטרול מערכות ובדיקת הנוזלים",
+        answer4: " בדיקת נזילות ותקינות המסבים",
+        correctAnswer: "c"
+            },
+            {
+        prompt: "מה נדרש לבצע אחרי ההתנעה",
+        answer1: " בדיקת לחץ שמן ופליטת מי-קירור לים",
+        answer2: " ניקוי ושימון מערכות",
+        answer3: " בדיקת מפלס הנוזלים ומילוי בהתאם לצורך )שמן,דלק, מים(",
+        answer4: " ניקוז אויר ומים",
+        correctAnswer:"a"
+            },
+            {
+        prompt: "מהם תפקידי טבעות הבוכנה",
+        answer1: " אטימת המרווח שבין הבוכנה לצילינדר",
+        answer2: " בלימת מהלך הבוכנה לפני הסיום",
+        answer3: " מניעת התרחבות הבוכנה",
+        answer4: " וויסות שחיקת הבוכנה",
+        correctAnswer:"a"
+        }, 
+        {
+        prompt: "מהו תפקידו העיקרי של גלגל התנופה",
+        answer1: " שמירת אינרציה ויציבות סלד בין הפעימות במחזור הפעולה" ,
+        answer2: " איזון התנועה הסיבובית של המתנע" ,
+        answer3: " נתינת תנופה לתיבת התשלובת",
+        answer4: " שמירה על סיבובים קצובים של המדחף",
+        correctAnswer:"a"
+        },
+        {
+        prompt: "מהם תפקידיו העיקריים של גל הארכובה",
+        answer1: " הפיכת תנועה סיבובית לקשתית",
+        answer2: " הפיכת תנועה קווית לסיבובית",
+        answer3: " האצת תנועת הבוכנות",
+        answer4: " בלימת התנועה המהירה של הבוכנות",
+        correctAnswer:"b",
+        },
+        {
+        prompt: "כיצד מתבצעת סיכה 'שימון' בלחץ",
+        answer1: " על ידי לחץ הידרוסטטי של השמן",
+        answer2: " בעזרת משאבת לחץ שמן ",
+        answer3: " על ידי כוח הגרוויטציה של השמן ",
+        answer4: " על ידי לחץ שמן צנטריפוגלי",
+        correctAnswer:"b"
+        },
+        {
+        prompt: "מהו סדר הפעולות במנוע דיזל בעל 4 פעימות ? ",
+        answer1: " יניקה, עבודה, דחיסה, פליטה. ",
+        answer2: " דחיסה, עבודה, יניקה, פליטה. ",
+        answer3: " עבודה, יניקה, דחיסה, פליטה. ",
+        answer4: " יניקה, דחיסה, עבודה, פליטה. ",
+        correctAnswer:"d"
+        },
+        {
+        prompt: "איזה מנגנון מבצע מתזמן את הפעלת השסתומים? ",
+        answer1: " גל הזיזים. ", 
+        answer3: " גל הארכובה. ",
+        answer2: " גלגל התנופה. ",
+        answer4: "שרשרת התזמון",
+        correctAnswer:"a"
+
+        },
+        {
+            prompt:"לאן מחוברות הבוכנות והטלטל במנוע",
+            answer1: "אל גל הארכובה",
+            answer3: " אל גל הזיזים",
+            answer2: " ישירות לציר",
+            answer4: " אל גלגל התנופה דרך הטלטל",
+            correctAnswer:"a"
+            },
+            {
+            
+            prompt:"כיצד משנים את מהירות סיבובי המנוע במנוע דיזל",
+            answer1: "על ידי שליטה בווסת הסיבובים",
+            answer2: " בעזרת משאבת ההזנה", 
+            answer3: " על ידי שליטה בווסת האוויר",
+            answer4: " בעזרת משאבת התאוצה",
+            correctAnswer:"a"
+            },
+            {
+            
+            prompt:"איזה גלגל שיניים אינו מסתובב בשעת פעולת מנוע רגילה",
+            answer1: "של גל התזמון",
+            answer2: " של המתנע",
+            answer3: " של מנגנון הבלימה",
+            answer4: " של מנגנון האוורור",
+            correctAnswer:"b"
+            },
+            {
+            
+            prompt:"איזה חלק במערכת הדלק קובע את כמות הדלק המוזרק במנוע דיזל",
+            answer1: "משאבת ההזרקה",
+            answer2: " המפלג " ,
+            answer3: " משאבת אספקת )הזנה( הדלק",
+            answer4: " טיימר ההזרקה",
+            correctAnswer:"a"
+            },
+            {
+            
+            prompt:"באיזה אזור במנוע נמדד לחץ השמן",
+            answer1: "בקו יניקת השמן",
+            answer2: " אחרי מסנן השמן",
+            answer3: " באגן השמן",
+            answer4: " לפני מסנן השמן",
+            correctAnswer:"b"
+            },
+            {
+            
+            prompt:"מהו תפקידה של הבוכנה",
+            answer1: "לאטום את חלל הצילינדר",
+            answer2: " להניע את השסתומים",
+            answer3: " להפוך את לחץ הבעירה לתנועה קווית",
+            answer4: " לינוק את התערובת",
+            correctAnswer:"c"
+            },
+            {
+            
+            prompt:"איך ניצת הדלק במנוע דיזל",
+            answer1: "על ידי החום שנוצר עקב דחיסת האוויר",
+            answer2: " על ידי ניצוץ חשמלי",
+            answer3: " על ידי החום שנוצר מחיכוך הבוכנה בדפנות הצילינדר",
+            answer4: " על ידי ניצוץ אלקטרומכני",
+            correctAnswer:"a"
+            },
+            {
+            
+            prompt:"מהו תפקידו של הטלטל",
+            answer1: "לרסן את טלטולי הבוכנות",
+            answer2: " לקשר בין הבוכנה לגל הארכובה",
+            answer3: " לטלטל את גל הזיזים",
+            answer4: " לקשר בין הבוכנה לגל הזיזים",
+            correctAnswer:"b"
+            },
+            {
+            
+            prompt:"מה דוחסת הבוכנה במנוע דיזל",
+            answer1: "טיפות סולר עם אוויר",
+            answer2: " אוויר עם אדי דלק )תערובת(", 
+            answer3: " אדי דלק בלבד ",
+            answer4: " אוויר בלבד",
+            correctAnswer:"d"
+            },
+            {
+            prompt:"מה מחובר בקצוות גל הארכובה",
+            answer1:" המצמד מצד אחד ומשאבת הדלק והקירור מצד שני",
+            answer2:" משאבת מי הים מצד אחד וגל הזיזים מצד שני",
+            answer3:" ציר המדחף ישירות מצד אחד והמתנע מצד שני",
+            answer4:" תיבת התשלובת דרך גלגל התנופה מצד אחד וגלגל רצועות מצד שני ",
+            correctAnswer: "d"
+            },
+            {
+
+            prompt:"כיצד מדמימים מנוע דיזל",
+            answer1:" מפסיקים את הספקת הדלק באמצעות ווסת הסיבובים",
+            answer2:" מנתקים את אספקת החשמל למשאבת ההזנה",
+            answer3:" מנתקים את אספקת האוויר",
+            answer4:" מנתקים את מנגנון השאיבה על ידי פיקוד מכני",
+            correctAnswer: "a"
+            },
+            {
+
+            prompt:"היכן מרוכז השמן במנוע דיזל",
+            answer1:" באגן השמן",
+            answer2:" במשאבה ובצנרת",
+            answer3:" במיכל הקליטה העליון",
+            answer4:" במיכל הקירור",
+            correctAnswer: "a"
+            },
+            {
+
+            prompt:"מהו תפקידו העיקרי של מפוח האוויר במנוע דיזל 2 פעימות",
+            answer1:" להכניס יותר אוויר לצילינדר לצורך הגברת הספק המנוע",
+            answer2:" לנצל עד תום את טמפרטורת גזי הפליטה",
+            answer3:" להגביר את מהירות המנוע",
+            answer4:" לדחוס אוויר טרי לצילינדר במקום פעולת היניקה המתרחשת במנוע 4 פעימות",
+            correctAnswer: "d"
+            },
+            {
+
+            prompt:"מהו תפקידו של מגדש הטורבו במנוע דיזל 4 פעימות",
+            answer1:" צמצום כמות הדלק המוזרקת ומכך חסכון משמעותי בתצרוכת הדלק",
+            answer2:" הגדלת הספק המנוע",
+            answer3:" הגדלת מהירות סיבוב המנוע על ידי לחץ אוויר בדחיסה לצילינדרים",
+            answer4:" לגדוש יותר אויר המסייע לחימום הדלק",
+            correctAnswer: "b"
+            },
+            {
+
+            prompt:"במה תלוי שינוי הספקו של מנוע דיזל ",
+            answer1:" בלחץ ההזרקה", 
+            answer2:" בסוג הדלק המוזרק",
+            answer3:" בכמות הדלק המוזרקת",
+            answer4:" במהירות המשאבה",
+            correctAnswer: "c"
+            },
+            {
+
+            prompt:"מהו יעודה הבסיסי של משאבה צנטריפוגלית במנוע",
+            answer1:" סחרור דלק",
+            answer2:" הזרקת שמן",
+            answer3:" שאיבת מים",
+            answer4:" סחרור אוויר",
+            correctAnswer: "c"
+            },
+            {
+
+            prompt:"מהו תפקיד משאבת ההזרקה",
+            answer1:" הספקת דלק בלחץ ובתזמון למרססי הדלק",
+            answer2:" הזרקת שמן לבוכנות",
+            answer3:" הזרקת מים בלחץ למערכת הקירור",
+            answer4:" הזרקת עודפי דלק למיכל",
+            correctAnswer: "a",
+            },
+            {
+            prompt:"למה מסוגלת משאבת בוכנה",
+            answer1:" מסוגלת לשאוב רק מים",
+            answer2:" מסוגלת לשאוב אוויר ונוזלים",
+            answer3:" מסוגלת לשאוב רק דלק",
+            answer4:" המשאבה חייבת לקבל לחץ ביניקה",
+            correctAnswer:"b"
+            },
+            {
+
+            prompt:"מהו עקרון הפעולה של משאבת בוכנה",
+            answer1:" נפח משתנה",
+            answer2:" תנועה סיבובית",
+            answer3:" לחץ משתנה",
+            answer4:" שטח משתנה",
+            correctAnswer:"a"
+             },
+            {
+
+            prompt:"מהם המצבים הנדרשים ממתג ההפעלה של משאבת השיפוליים האוטומטית",
+            answer1:" כיבוי, תפוקה נמוכה, תפוקה גבוהה",
+            answer2:" כיבוי, מופעל ידנית, מופעל אוטומטית )מצוף)",
+            answer3:" כיבוי, מופעל מצוף, מופעל אוטומטית",
+            answer4:" כיבוי, נעול, מופעל אוטומטית",
+            correctAnswer:"b" 
+            },
+            {
+            prompt:"מהי התקלה הנפוצה במשאבת מי-ים",
+            answer1:" חדירת אוויר למערכת",
+            answer2:" חסימת פתח היניקה",
+            answer3:" חסימת פתח הפליטה",
+            answer4:" חוסר סנכרון עם המנוע",
+            correctAnswer:"b"
+             },
+            {
+            prompt:"מהו עקרון הפעולה של משאבה צנטריפוגלית",
+            answer1:" המהירות הצנטריפוגלית הופכת לתנועה סיבובית",
+            answer2:" הנוזל נזרק למרכז המשאבה עקב התנועה הסיבובית",
+            answer3:" הנוזל נזרק לדפנות המשאבה עקב התנועה הסיבובית",
+            answer4:" המהירות הצנטריפוגלית הופכת לכוח",
+            correctAnswer:"c"
+             },
+            {
+            prompt:"מהו השימוש הנפוץ של משאבת הטבילה הניידת",
+            answer1:" שאיבת מי-שתייה",
+            answer2:" שאיבת שמן טבול",
+            answer3:" שאיבת דלק בחירום מתחתית המיכל",
+            answer4:" שאיבת מי שיפוליים בחירום",
+            correctAnswer:"d"
+             },
+            {
+            prompt:"מהי משאבת ההזנה הנפוצה במערכת הדלק של המנוע",
+            answer1:" משאבת דיאפרגמה",
+            answer2:" משאבת רוטור",
+            answer3:" משאבת אימפלר",
+            answer4:" משאבת בוכנה",
+            correctAnswer:"a" },
+            {
+            prompt:"על איזה עקרון פועלת משאבת אימפלר גומי )ג'בסקו(",
+            answer1:" שינוי לחץ", 
+            answer2:" שינוי מהירות",
+            answer3:" שינוי נפח",
+            answer4:" כוח צנטריפוגלי",
+            correctAnswer:"c" },
+            {
+            prompt:"מה יכולה להיות הסיבה שמשאבת השיפוליים הצנטריפוגלית איננה מצליחה לפלוט את המים לים",
+            answer1:" ההתנגדות הצנטריפוגלית קטנה מדי",
+            answer2:" הלחץ ההידרוסטטי בפליטה נמוך מדי",
+            answer3:" העומס הצנטריפוגלי גדול מדי",
+            answer4:" הלחץ ההידרוסטטי בפליטה גבוה מדי", 
+            correctAnswer:"d" },
+
+
+
+
+
+        
+
+
+
+]
+
+
+
+// DOM Selectors
+const questionAsked = document.querySelector("#question");
+const answer1 = document.querySelector("#a");
+const answer2 = document.querySelector("#b");
+const answer3 = document.querySelector("#c");
+const answer4 = document.querySelector("#d");
+const start = document.querySelector("#start");
+const ansers = document.querySelectorAll("span");
+const circle = document.querySelector("#new");
+
+
+
+// VARs
+let i = 0;
+let score = 0;
+let tryagian = 0;
+var qna = [];
+let answer;
+
+
+
+// Ask question from the list according to the array of random 10 questions
+function askQuestoion () {
+    
+    if ( i < 10) {
+
+        questionAsked.textContent = question[qna[i]].prompt;
+        answer1.textContent = question[qna[i]].answer1;
+        answer2.textContent = question[qna[i]].answer2;
+        answer3.textContent = question[qna[i]].answer3;
+        answer4.textContent = question[qna[i]].answer4;
+        start.textContent = i + " / 10 "
+
+    } else {
+        i=0
+
+        circle.textContent = "השאלון נגמר ענית " + score + " תשובות נכונות מתוך 10";
+        score = 0;
+        qna = [];
+    }
+
+
+    //The click set the value of the answer and than run the checking function that check if the answer is correct
+}
+
+
+function set1 () {
+    answer = "a"
+    check()
+} 
+function set2 () {
+    answer = "b"
+    check()
+} 
+function set3 () {
+    answer = "c"
+    check()
+} 
+function set4 () {
+    answer = "d"
+    check()
+}
+
+//this function check if the answer is correct and than move to the next question. if the answer is correct the score is added and the correct 
+
+function check () {
+    if (answer == question[qna[i]].correctAnswer) {
+        i++;
+        score++;
+        ansers[4].id = "ans";
+        ansers[4].textContent = "Correct";
+        askQuestoion();
+               
+    } else if (tryagian = 0) {
+        ansers[4].id = "ans1";
+        ansers[4].textContent = "Not Correct try agian";
+          
+    } else {
+         i++;
+         tryagian = 0;
+         ansers[4].id = "ans1";
+         ansers[4].textContent = "Not Correct";
+         askQuestoion();    
+
+    }
+
+}
+
+//Create array of randomize not reapting question
+function getRandomArray() {
+    
+    const getRanndomQuestio = Math.trunc (Math.random() * (question.length - 1) + 1);
+    qna.push(getRanndomQuestio);
+        for (let c=0; c<9; c++) {
+            let newGen = Math.trunc (Math.random() * (question.length - 1) + 1);
+            while (qna.lastIndexOf(newGen) !== -1) {
+                newGen = Math.trunc (Math.random() * (question.length - 1) + 1);
+            }
+        qna.push(newGen);
+        }
+        console.log(qna)
+        
+}
+
+
+
+// Press to start game with CSS animation
+
+function start_new () {
+    document.querySelector(".wrapper").style.visibility="visible";
+    document.querySelector(".wrapper").style.animation="transitionIn 2.2s";
+    circle.textContent="בהצלחה";
+    getRandomArray();
+    askQuestoion();
+    }
+
+// start.addEventListener('click', () => {
+//     getRandomArray();
+//     askQuestoion();
+// });
+
+
+answer1.addEventListener('click', set1, false);
+answer2.addEventListener('click', set2, false);
+answer3.addEventListener('click', set3, false);
+answer4.addEventListener('click', set4, false);
+
+
+
